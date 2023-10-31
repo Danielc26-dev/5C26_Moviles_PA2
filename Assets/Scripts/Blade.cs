7e7be1eb2255ec5bdc4ecee3f0deac31a1ab96c4
Maria Claverias <143734728+mariaclaverias@users.noreply.github.com>
2023-10-31 15:42:38 -0500

    A_1A_2A

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -14,6 +14,10 @@ public class Blade : MonoBehaviour
 
     private bool slicing;
 
+    private Vector2 startTouch;
+    private Vector2 EndTouch;
+
+
     private void Awake()
     {
         mainCamera = Camera.main;
@@ -33,18 +37,61 @@ public class Blade : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            StartSlice();
-        } else if (Input.GetMouseButtonUp(0)) {
-            StopSlice();
-        } else if (slicing) {
-            ContinueSlice();
+        //if (Input.GetMouseButtonDown(0)) {
+        //    StartSlice();
+        //} else if (Input.GetMouseButtonUp(0)) {
+        //    StopSlice();
+        //} else if (slicing) {
+        //    ContinueSlice();
+        //}
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 position = Camera.main.ScreenToWorldPoint(touch.position);
+            transform.position = position;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                startTouch = touch.position;
+
+                RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+
+                if (touch.tapCount == 1)
+                {
+                    slicing = true;
+                    sliceCollider.enabled = true;
+                    sliceTrail.enabled = true;
+                    sliceTrail.Clear();
+                }
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                //if (currentObject != null)
+                //{
+                //    currentObject.transform.position = position;
+                //    isDrag = true;
+                //}
+                //else
+                //{
+                //    isDrag = false;
+                //}
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                EndTouch = touch.position;
+
+                slicing = false;
+                sliceCollider.enabled = false;
+                sliceTrail.enabled = false;
+            }
         }
     }
 
     private void StartSlice()
     {
-        Vector3 position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 position = mainCamera.ScreenToWorldPoint(touch.position);
         position.z = 0f;
         transform.position = position;
 
@@ -63,7 +110,7 @@ public class Blade : MonoBehaviour
 
     private void ContinueSlice()
     {
-        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(touch.position);
         newPosition.z = 0f;
 
         direction = newPosition - transform.position;
