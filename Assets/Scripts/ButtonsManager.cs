e072933a5135d307680af13ea8c6fa53d6f0195d
DorcasGacel <tabita.acuna@tecsup.edu.pe>
2023-10-31 16:14:16 -0500

    Menu

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ButtonsManager : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene("FruitNinja");
+    }
+
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Salí");
+    }
+}
